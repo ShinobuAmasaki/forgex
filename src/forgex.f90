2b4294bcb72bbffb408e12325b9644c0107fabb0
Amasaki Shinobu <amasaki203@gmail.com>
2023-12-19 23:36:21 +0900

    [mod] forgex_m accepts in_operator_m.

--- src/forgex.f90
+++ src/forgex.f90
@@ -11,7 +11,8 @@ module forgex
    use, intrinsic :: iso_fortran_env, stderr=>error_unit
    
    use :: syntax_tree_m, only: parse_regex, tree_t, print_tree, deallocate_tree
-   use :: automaton_m 
+   use :: automaton_m
+   use :: in_operator_m
 
    type(automaton_t) :: automaton
 
