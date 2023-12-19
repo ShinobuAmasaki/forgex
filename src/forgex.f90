3637a3abed6f31966ee1cf390de1fbdc9caa3249
Amasaki Shinobu <amasaki203@gmail.com>
2023-12-19 16:54:04 +0900

    [add] a subroutine deallocate_tree introduced.

--- src/forgex.f90
+++ src/forgex.f90
@@ -5,14 +5,15 @@
 !! (C) Amasaki Shinobu, 2023
 !!     A regular expression engine for Fortran.
 !!
+!!     forgex_m defines APIs of Forgex.
+!!  
 module forgex
    use, intrinsic :: iso_fortran_env, stderr=>error_unit
    
-   use :: syntax_tree_m, only: parse_regex, tree_t, print_tree
+   use :: syntax_tree_m, only: parse_regex, tree_t, print_tree, deallocate_tree
    use :: automaton_m 
 
-   ! use :: dfa_m, only: convert_nfa_to_dfa, print_dfa, matching
-   ! implicit none
+   type(automaton_t) :: automaton
 
 contains
 
