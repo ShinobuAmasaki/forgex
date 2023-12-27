dfd54c65c143a63af75830bba53f9f840704d0dc
Amasaki Shinobu <amasaki203@gmail.com>
2023-12-27 17:11:54 +0900

    [add] feature from/to arguemnts in the regex function.

--- src/forgex.f90
+++ src/forgex.f90
@@ -140,20 +140,21 @@ contains
    end function match__matching
 
 
-   function regex__matching (pattern, str, length) result(res)
+   function regex__matching (pattern, str, length, from, to) result(res)
       implicit none
       character(*), intent(in) :: pattern, str
       integer(int32), intent(inout), optional :: length
+      integer(int32), intent(inout), optional :: from, to
       character(:), allocatable :: res
 
       character(:), allocatable :: buff
-      integer(int32) :: from, to
+      integer(int32) :: from_l, to_l
 
       type(tree_t), pointer :: root
       type(tape_t) :: tape
    
-      from = 0
-      to = 0
+      from_l = 0
+      to_l = 0
 
       buff = pattern
 
@@ -172,27 +173,31 @@ contains
 
       ! call cache%print_dfa()
       
-      call cache%matching(char(10)//str//char(10), from, to)
+      call cache%matching(char(10)//str//char(10), from_l, to_l)
 
 
       if (is_there_caret_at_the_top(pattern)) then
-         from = from
+         from_l = from_l
       else 
-         from = from - 1
+         from_l = from_l - 1
       end if
 
       if (is_there_dollar_at_the_end(pattern)) then
-         to = to - 2
+         to_l = to_l - 2
       else
-         to = to - 1
+         to_l = to_l - 1
       end if
 
-      if (from > 0 .and. to > 0) then
-         res = str(from:to)
-         if (present(length)) length = to - from + 1
+      if (from_l > 0 .and. to_l > 0) then
+         res = str(from_l:to_l)
+         if (present(length)) length = to_l - from_l + 1
+         if (present(from)) from = from_l
+         if (present(to)) to = to_l
       else
          res = ''
          if (present(length)) length = 0
+         if (present(from)) from = 0
+         if (present(to)) to = 0
       end if
 
       call deallocate_tree()
