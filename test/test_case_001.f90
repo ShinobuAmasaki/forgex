0dc586be577c10f2f3d1ac747f3abd9d836ddca6
Amasaki Shinobu <amasaki203@gmail.com>
2023-12-27 20:37:19 +0900

    [add] test cases to 001 and 002.

--- test/test_case_001.f90
+++ test/test_case_001.f90
@@ -5,6 +5,7 @@ program test_001
 
    logical :: res = .true.
 
+print *, 'hoge'
    ! Test case #1. 
    ! Tests for ASCII Character set.
    call runner_match('[a]{1,4}', 'aaaaa', .false., res)
@@ -33,6 +34,41 @@ program test_001
    call runner_match('[a]{,4}', 'a', .true., res)
    call runner_match('[a]{,4}', '', .true., res)
 
+   call runner_match('[a-z]{,7}', 'aaaaaaaa', .false., res)
+   call runner_match('[a-z]{,7}', 'aaaaaaa', .true., res)
+   call runner_match('[a-z]{,7}', 'aaaaaa', .true., res)
+   call runner_match('[a-z]{,7}', 'aaaaa', .true., res)
+   call runner_match('[a-z]{,7}', 'aaaa', .true., res)
+   call runner_match('[a-z]{,7}', 'aaa', .true., res)
+   call runner_match('[a-z]{,7}', 'aa', .true., res)
+   call runner_match('[a-z]{,7}', 'a', .true., res)
+   call runner_match('[a-z]{,7}', '', .true., res)
+
+   call runner_match('[a-z]{2,7}', 'aaaaaaaa', .false., res)
+   call runner_match('[a-z]{2,7}', 'aaaaaaa', .true., res)
+   call runner_match('[a-z]{2,7}', 'aaaaaa', .true., res)
+   call runner_match('[a-z]{2,7}', 'aaaaa', .true., res)
+   call runner_match('[a-z]{2,7}', 'aaaa', .true., res)
+   call runner_match('[a-z]{2,7}', 'aaa', .true., res)
+   call runner_match('[a-z]{2,7}', 'aa', .true., res)
+   call runner_match('[a-z]{2,7}', 'a', .false., res)
+   call runner_match('[a-z]{2,7}', '', .false., res)
+
+   call runner_regex('[a-z]{1,7}', 'aaaaaaab', 'aaaaaaa', res)
+   call runner_regex('[a-z]{1,6}', 'aaaaaaab', 'aaaaaa', res)
+   call runner_regex('[a-z]{1,5}', 'aaaaaaab', 'aaaaa', res)
+   call runner_regex('[a-z]{1,4}', 'aaaaaaab', 'aaaa', res)
+   call runner_regex('[a-z]{1,3}', 'aaaaaaab', 'aaa', res)
+   call runner_regex('[a-z]{1,2}', 'aaaaaaab', 'aa', res)
+   call runner_regex('[a-z]{1,1}', 'aaaaaaab', 'a', res)
+
+   call runner_regex('[a-z]{,7}', 'aaaaaaab', 'aaaaaaa', res)
+   call runner_regex('[a-z]{,6}', 'aaaaaaab', 'aaaaaa', res)
+   call runner_regex('[a-z]{,5}', 'aaaaaaab', 'aaaaa', res)
+   call runner_regex('[a-z]{,4}', 'aaaaaaab', 'aaaa', res)
+   call runner_regex('[a-z]{,3}', 'aaaaaaab', 'aaa', res)
+   call runner_regex('[a-z]{,2}', 'aaaaaaab', 'aa', res)
+   call runner_regex('[a-z]{,1}', 'aaaaaaab', 'a', res)
    
    call runner_match('[a]{2, 4}', "aaaaa", .false., res)
    call runner_match('[a]{2, 4}', "aaaa", .true., res)
--- test/test_case_002.f90
+++ test/test_case_002.f90
@@ -8,14 +8,24 @@ program test_002
    ! Test case #2. 
    ! Tests for UTF-8 Character set.
 
+
    call runner_in('[い]{7,7}', 'いろはにほへとち', .false., res)
+
    call runner_match("[い]{6}", 'いいいいいい ', .false., res)
    call runner_in("[^さ-ん]{6}",  'あいうえおか', .true., res)
-   call runner_in("夢.{1,7}胡蝶", "昔者莊周夢爲胡蝶　栩栩然胡蝶也", .true., res)
 
+   call runner_regex('[ぁ-ん]{1,7}', 'いろはにほへとちりぬるを', 'いろはにほへと', res)
+
+   call runner_regex('[ぁ-ん]{,7}', 'いろはにほへとちりぬるを', 'いろはにほへと', res)
+
+
+   call runner_in("夢.{1,7}胡蝶", "昔者莊周夢爲胡蝶　栩栩然胡蝶也", .true., res)
+   call runner_regex('夢.{1,7}胡蝶', "昔者莊周夢爲胡蝶　栩栩然胡蝶也", "夢爲胡蝶　栩栩然胡蝶", res )
+
+   ! Zenkaku space
    call runner_match("\s", '　', .true., res)
    call runner_match('\s*', '　　', .true., res)
-
+100 continue
 
    if (res) then
       print *, "=== TEST CASE 2 END ===          "
