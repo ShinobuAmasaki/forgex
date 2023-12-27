3c710f2a0f0a31cbfadd44db12da6674dc010d11
Amasaki Shinobu <amasaki203@gmail.com>
2023-12-27 22:06:53 +0900

    [mod] test_m outputs more verbosely.

--- src/test_m.f90
+++ src/test_m.f90
@@ -6,8 +6,11 @@ module test_m
 
    public :: is_valid__in
    public :: is_valid__match
+   public :: is_valid__regex
    public :: runner_in
    public :: runner_match
+   public :: runner_regex
+
 contains
 
    function is_valid__in(pattern, str, correct_answer) result(res)
@@ -29,6 +32,24 @@ contains
       res = (pattern .match. str) .eqv. correct_answer
    end function is_valid__match
 
+   function is_valid__regex(pattern, str, answer, substr) result(res)
+      implicit none
+      character(*), intent(in) :: pattern, str
+      character(*), intent(in) :: answer
+      character(:), allocatable, intent(inout) :: substr
+      character(:), allocatable :: local
+      integer(int32) :: length
+      logical :: res 
+
+
+      local = regex(pattern, str, length)
+
+      substr = local
+
+      res = trim(local) == trim(answer)
+
+   end function is_valid__regex
+
    subroutine runner_in(pattern, str, answer, result)
       implicit none
       character(*), intent(in) :: pattern, str
@@ -40,11 +61,10 @@ contains
 
       res = is_valid__in(pattern, str, answer)
 
-      write(error_unit, '(a)', advance='no') '                                          '//char(13)
       if (res) then
-         write(error_unit, '(a, l, a, a)', advance='no') 'result: ', res, ' '//trim(pattern),  char(13)
+         write(error_unit, '(a, a, a)') 'result(in   ): Success', ' '//trim(pattern)
       else
-         write(error_unit, '(a, l, a, a)') 'result: ', res, ' '//trim(pattern), ' '//trim(str)
+         write(error_unit, '(a, a, a)') 'result(in   ): FAILED ', ' '//trim(pattern), ' '//trim(str)
       end if
 
       result = result .and. res
@@ -62,16 +82,41 @@ contains
 
       res = is_valid__match(pattern, str, answer)
 
-      write(error_unit, '(a)', advance='no') '                                          '//char(13)
+      ! write(error_unit, '(a)', advance='no') '                                          '//char(13)
       if (res) then
-         write(error_unit, '(a, l, a, a)', advance='no') 'result: ', res, ' '//trim(pattern), char(13)
+         write(error_unit, '(a, a, a)') 'result(match): Success', ' '//trim(pattern), ' "'//trim(str)//'"'
       else
-         write(error_unit, '(a, l, a, a)') 'result: ', res, ' '//trim(pattern),' '//trim(str)
+         write(error_unit, '(a, a, a)') 'result(match): FAILED ' , ' '//trim(pattern),' "'//trim(str)//'"'
       end if
 
       result = result .and. res
 
    end subroutine runner_match
+
+
+   subroutine runner_regex(pattern, str, answer, result)
+      implicit none
+      character(*), intent(in) :: pattern, str
+      character(*), intent(in) :: answer
+      logical, intent(inout) :: result
+
+      character(:), allocatable :: substr
+
+      logical :: res
+
+      res = is_valid__regex(pattern, str, answer, substr)
+
+      ! write(error_unit, '(a)', advance='no') '                                          '//char(13)
+
+      if (res) then
+         write(error_unit, '(a, a, a)') 'result(regex): Success', ' '//trim(pattern), ' "'//trim(substr)//'"'
+      else
+         write(error_unit, '(a, a, a)') 'result(regex): FAILED ', ' '//trim(pattern), ' "'//trim(substr)//'"'
+      end if
+
+      result = result .and. res
+
+   end subroutine runner_regex
 
 
 end module test_m
