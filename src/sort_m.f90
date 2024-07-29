09040ea16b24de97c3976c530c6d8760f7a2d540
Amasaki Shinobu <amasaki203@gmail.com>
2024-07-29 23:39:59 +0900

    [add] insertion sort is available, not implemented into the engine.

--- src/sort_m.f90
+++ src/sort_m.f90
@@ -41,4 +41,24 @@ contains
 
    end subroutine bubble_sort
 
+
+   pure subroutine insertion_sort(list)
+      implicit none
+      integer, intent(inout) :: list(:)
+
+      integer :: i, j, key
+
+      do i = 2, size(list, dim=1)
+         key = list(i)
+         j = i - 1
+
+         do while(j > 0 .and. list(j) > key)
+            list(j+1) = list(j)
+            j = j - 1
+            if (j == 0) exit
+         end do
+         list(j + 1) = key
+      end do
+   end subroutine insertion_sort
+
 end module forgex_sort_m
