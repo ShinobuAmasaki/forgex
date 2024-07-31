87ef3a9af5a9963576b38721ee75112187ff507d
Amasaki Shinobu <amasaki203@gmail.com>
2024-07-31 20:11:09 +0900

    [mod] app/main.f90 modified for gprof.

--- app/main.f90
+++ app/main.f90
@@ -3,7 +3,14 @@ program main
    use :: forgex_time_measurement_m
    implicit none
    
-   call time_begin
-   print *, '\d{3}-\d{3}-\d{3}-\d{3}-\d{3}-\d{3}' .in. '123-456-789-123-456-789'
-   call time_end(".in. operator")
+   logical :: res, entire
+
+   entire = .true.
+   do i = 1, 1000
+      res = '\d{3}-\d{3}-\d{3}-\d{3}-\d{3}-\d{3}' .in. '123-456-789-123-456-789'
+      entire = entire .and. res
+   end do
+
+   print *, entire 
+   
 end program main
