21c6e450a34b17e27e5fd196fd66d7f1757f4012
Amasaki Shinobu <amasaki203@gmail.com>
2024-07-31 17:36:42 +0900

    [mod] main.f90

--- app/main.f90
+++ app/main.f90
@@ -5,5 +5,5 @@ program main
    
    call time_begin
    print *, '\d{3}-\d{3}-\d{3}-\d{3}-\d{3}-\d{3}' .in. '123-456-789-123-456-789'
-   call time_lap(".in. operator")
+   call time_end(".in. operator")
 end program main
