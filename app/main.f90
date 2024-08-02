dd49cae7c39d72b38919086303dbdd5a3a321255
Amasaki Shinobu <amasaki203@gmail.com>
2024-08-02 10:56:09 +0900

    [update] remove whitespaces from the end of a line.

--- app/main.f90
+++ app/main.f90
@@ -2,13 +2,13 @@ program main
    use :: forgex
    use :: forgex_time_measurement_cpu_m
    implicit none
-   
+
    logical :: res, entire
    integer :: i
 
    entire = .true.
    call time_begin()
-   
+
    do i = 1, 1000
       res = 'a[^x]{20}b' .in. 'aaakkkkkkkkkkkkkkkkkkkb'
       entire = entire .and. res
@@ -18,6 +18,6 @@ program main
    call time_lap("")
    call time_end("1000 loops .in. operator")
 
-   print *, entire 
-   
+   print *, entire
+
 end program main
