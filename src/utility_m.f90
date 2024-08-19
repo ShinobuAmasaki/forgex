73b031b9d2a722ceae3d2a5947d6db7f3cd343cd
Amasaki Shinobu <amasaki203@gmail.com>
2024-08-19 16:35:36 +0900

    [add] utility procedure get_index_list_forward.

--- src/utility_m.f90
+++ src/utility_m.f90
@@ -12,6 +12,7 @@ module forgex_utility_m
 
    public :: is_there_caret_at_the_top
    public :: is_there_dollar_at_the_end
+   public :: get_index_list_forward
 
 contains
 
@@ -49,5 +50,36 @@ contains
       res = buff(len_trim(buff):len_trim(buff)) == '$'
    end function is_there_dollar_at_the_end
 
+
+   pure subroutine get_index_list_forward(text, prefix, index_array)
+      use, intrinsic :: iso_fortran_env, only: int32
+      implicit none
+      character(*), intent(in) :: text, prefix
+      integer(int32), allocatable, intent(inout) :: index_array(:)
+      
+      integer :: offset, idx, len_pre
+
+      len_pre = len(prefix)
+      if (len_pre == 0) then
+         return
+      end if
+
+      if (allocated(index_array)) deallocate(index_array)
+      idx = index(text, prefix)
+      if (idx <= 0) then
+         index_array = [0]
+         return
+      else
+         index_array = [idx]
+      end if
+
+      offset = idx + len_pre -1
+      do while (offset < len(text))
+         idx = index(text(offset+1:), prefix)
+         if (idx <= 0) exit
+         index_array = [index_array, idx + offset]
+         offset = offset + idx + len_pre -1
+      end do
+   end subroutine get_index_list_forward
    
 end module forgex_utility_m
