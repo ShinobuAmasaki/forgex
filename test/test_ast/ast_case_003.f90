e6b167acf2eae9bb43887646a1855ac78339c6f8
Amasaki Shinobu <amasaki203@gmail.com>
2024-08-22 16:37:26 +0900

    [add] test cases for get_middle_literal in ast_case_003.

--- test/test_ast/ast_case_003.f90
+++ test/test_ast/ast_case_003.f90
@@ -7,8 +7,8 @@ program ast_case_003
    call runner_middle("[ab]*a[ab]{20}", "a", res)
    call runner_middle("[a]{3,4}", "aaa", res)
    call runner_middle("(a|b)+", "", res)
-   call runner_middle("(b?)a*(cd){3,4}", "", res)
-   call runner_middle("(b+)a*(cd){3,4}", "", res)
+   call runner_middle("(b?)a*(cd){3,4}", "cdcdcd", res)
+   call runner_middle("(b+)a*(cd){3,4}", "cdcdcd", res)
    call runner_middle("(b?)a+(cd){3,4}", "a", res)
    call runner_middle("z(ab|ac|a+){3,4}", "", res)
    call runner_middle("z(ab|ac){3,4}", "", res)
@@ -20,6 +20,22 @@ program ast_case_003
    call runner_middle("c{2,3}", "cc", res)
    call runner_middle("c{1,3}", "c", res)
    call runner_middle("c{0,3}", "", res)
+
+   call runner_middle("ab*c+d", "c", res)
+   call runner_middle("a(cd)+e", "cd", res)
+   call runner_middle("x(ab|cd)y", "y", res)
+   call runner_middle("a(bc|de)f", "f", res)
+   call runner_middle("ab*(cd)+ef", "cd", res)
+   call runner_middle("(ab|cd)efg", "efg", res)
+   call runner_middle("(abc|def)gh", "gh", res)
+   call runner_middle("a(bc|de)fg", "fg", res)
+   call runner_middle("x(yz|ab)cd", "cd", res)
+   call runner_middle("a(b(cd)+)e", "cd", res)
+   call runner_middle("x(y*z(cd)+ef)g", "zcd", res)
+   call runner_middle("a(bc|de)(fg)+h", "fg", res)
+   call runner_middle("x(y|z)*ab", "ab", res)
+   call runner_middle("a(bc)+d", "bc", res)
+   call runner_middle("(ab)*c(de)+f", "cde", res)
 
 
    if (res) then
