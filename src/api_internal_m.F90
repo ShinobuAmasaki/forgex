f57cd4709b3741bde65167ccf3a08c16569466d7
Amasaki Shinobu <amasaki203@gmail.com>
2025-02-27 12:41:53 +0900

    [mod] forgex_api_internal_m uses next_idxutf8 procedure.

--- src/api_internal_m.F90
+++ src/api_internal_m.F90
@@ -17,7 +17,7 @@ module forgex_api_internal_m
    use, intrinsic :: iso_fortran_env, only: stderr => error_unit
    use :: forgex_parameters_m, only: DFA_NOT_INIT, DFA_INVALID_INDEX
    use :: forgex_automaton_m, only: automaton_t
-   use :: forgex_utf8_m, only: idxutf8
+   use :: forgex_utf8_m, only: next_idxutf8
    implicit none
    private
 
@@ -124,7 +124,7 @@ contains
 
             if (ci > len(str)) exit
 
-            next_ci = idxutf8(str, ci) + 1
+            next_ci = next_idxutf8(str, ci)
 
             call automaton%construct(cur_i, dst_i, str(ci:next_ci-1))
 
@@ -145,7 +145,7 @@ contains
          end if
 
          if (do_brute_force) then
-            start = idxutf8(str, start) + 1 ! Bruteforce searching
+            start = next_idxutf8(str, start) ! Bruteforce searching
             cycle
          endif
 
@@ -260,7 +260,7 @@ contains
          if (ci > len(str)) exit
 
          ! Get the index of the next character and assign it to `next_ci`.
-         next_ci = idxutf8(str, ci) + 1
+         next_ci = next_idxutf8(str, ci)
 
          ! Lazy evaluation is performed by calling this procedure here.
          ! The index of destination DFA node is stored in the `dst_i` variable.
@@ -269,7 +269,7 @@ contains
          ! If there is mismatch in the first byte of the NULL character, try again with the second byte.
          if (dst_i == DFA_INVALID_INDEX .and. ci == 1) then
             ci = 2
-            next_ci = idxutf8(str, ci) + 1
+            next_ci = next_idxutf8(str, ci)
             call automaton%construct(cur_i, dst_i, str(ci:next_ci-1))
          end if
 
