bb39efd1f8221ef3f5e4ded7f7879f019cfae991
Amasaki Shinobu <amasaki203@gmail.com>
2025-03-08 11:50:32 +0900

    [update] optimize_m passed ast_case_001.

--- src/ast/syntax_tree_optimize_exp_m.F90
+++ src/ast/syntax_tree_optimize_exp_m.F90
@@ -26,6 +26,7 @@ module forgex_syntax_tree_optimize_exp_m
    type literal_t
       type(character_array_element_t) :: all, pref, suff, fact
       logical :: flag_closure = .false.
+      logical :: flag_class = .false.
    end type literal_t
 
    character(0), parameter :: theta = ''
@@ -67,6 +68,7 @@ contains
 
       type(literal_t) :: lit_l, lit_r
       type(tree_node_t) :: curr
+      integer :: i
 
       curr = nodes(idx)
 
@@ -88,13 +90,20 @@ contains
 
       select case (curr%op)
       case(op_union)
-         lit%all%c =  theta
-         lit%pref%c = theta
-         lit%suff%c = theta
+         lit%all%c =  theta 
+         lit%pref%c = same_part_of_prefix(lit_l%pref%c, lit_r%pref%c)
+         lit%suff%c = theta !!
          lit%fact%c = theta
          lit%flag_closure = .true.
-
       case(op_concat)
+
+         if (lit_l%flag_class .or. lit_r%flag_class) then
+            lit%all%c = theta
+            lit%pref%c = theta
+            lit%suff%c = theta
+            lit%fact%c = theta
+            return
+         end if
 
          if (lit_l%flag_closure .and. lit_r%flag_closure) then 
             lit%all%c = theta
@@ -115,6 +124,7 @@ contains
             lit%all%c = lit_l%all%c//lit_r%all%c
             lit%pref%c = best(lit_l%pref%c, lit_l%all%c//lit_r%pref%c)
             lit%suff%c = best(lit_r%suff%c, lit_l%suff%c//lit_r%all%c)
+            lit%flag_closure = lit_l%flag_closure .or. lit_r%flag_closure
          end if 
 
          lit%fact%c = best(best(lit_l%fact%c, lit_r%fact%c), lit_l%suff%c//lit_r%pref%c)
@@ -138,16 +148,37 @@ contains
                lit%pref%c = theta
                lit%suff%c = theta
                lit%fact%c = theta
+               lit%flag_class = .true.
             end if
+         else
+            lit%all%c = theta
+            lit%pref%c = theta
+            lit%suff%c = theta
+            lit%fact%c = theta
          end if
       case (op_repeat)
-         !== STUB CODE
-         lit%all%c =  theta
-         lit%pref%c = theta
-         lit%suff%c = theta
-         lit%fact%c = theta
-         lit%flag_closure = .true.
-         !== END OF STUB
+
+         do i = 1, curr%min_repeat
+            if (lit%flag_class .or. lit_r%flag_class) then
+               lit%all%c = theta
+               lit%pref%c = theta
+               lit%suff%c = theta
+               lit%fact%c = theta
+               exit
+            else
+               call best_factor(nodes, curr%left_i, lit_l)
+               lit%all%c = lit%all%c//lit_l%all%c
+               lit%pref%c = lit%pref%c//lit_l%pref%c
+               lit%suff%c = lit%suff%c//lit_l%suff%c
+               lit%fact%c = lit%fact%c//lit_l%fact%c
+            end if
+            if (lit_l%flag_closure) exit
+
+         end do
+
+         lit%flag_closure = curr%min_repeat /= curr%max_repeat
+         lit%flag_closure = lit%flag_closure .or. lit_l%flag_closure .or. lit_r%flag_closure
+      
       case default
          lit%all%c =  theta
          lit%pref%c = theta
@@ -171,5 +202,38 @@ contains
          res = trim(adjustl(c2))
       end if
    end function best
+
+
+   pure function same_part_of_prefix (c1, c2) result(res)
+      use :: forgex_utf8_m
+      implicit none
+      character(*), intent(in) :: c1, c2
+
+      character(:), allocatable :: res, part1, part2
+      logical :: flag_return
+      integer :: i
+      res = ''
+
+      i = 1
+      flag_return = .false.
+      do while(.not. flag_return)
+
+         part1 = c1(i:idxutf8(c1, i))
+         part2 = c2(i:idxutf8(c2, i))
+         flag_return = next_idxutf8(c1, i) == INVALID_CHAR_INDEX .or. next_idxutf8(c2,i) == INVALID_CHAR_INDEX
+
+         if (flag_return) return
+
+         if (part1 == part2) then
+            res = res//part1
+         else
+            exit
+         end if
+
+         i = next_idxutf8(c1, i)
+      end do
+      
+   end function same_part_of_prefix
+
 
 end module forgex_syntax_tree_optimize_exp_m
