87155fb49c6a7f123848e038938dd5e5f4f0ae8d
Amasaki Shinobu <amasaki203@gmail.com>
2026-07-10 01:51:01 +0900

    [add] minimal tests for the fixes to 329a00b0 and 7a3c2d08

--- test/test_api/test_case_015.f90
+++ test/test_api/test_case_015.f90
@@ -7,12 +7,20 @@ program test_case_015
    res = .true.
    print *, '=== TEST CASE 15 BEGIN ==='
 
+   ! fixed on the 329a00b0 commit
    call runner_match("abc\d", "abc", .false., res)
    call runner_match("abc\d", "abc1", .true., res)
    call runner_match("abc\d+", "abc1", .true., res)
    call runner_match("abc[0-9]", "abc1", .true., res)
    call runner_match("abc\w", "abcd", .true., res)
    call runner_match("abc\w", "abc", .false., res)
+   
+   ! fixed on the 7a3c2d08 commit 
+   call runner_in(".", ' ', .true., res)
+   call runner_in("\s", ' ', .true., res)
+   call runner_in("\S", ' ', .false., res)
+   call runner_match("\s", ' ', .true., res)
+   call runner_match(".", ' ', .true., res)
 
    if (res) then
       write(error_unit, *) '=== TEST CASE 15 END ==='
